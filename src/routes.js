9ef833e838e6eec74db2927f7916e8f18009736b
leojaimesson <leo.jaimesson.dev@gmail.com>
2019-10-19 16:10:04 -0300

    create loader models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (request, response) => response.json({ hello: 'World' }));
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    name: 'leo jaimesson',
+    email: 'leo.jaimesson@gmail.com',
+    password_hash: '12345',
+  });
+  response.json(user);
+});
 
 export default routes;
